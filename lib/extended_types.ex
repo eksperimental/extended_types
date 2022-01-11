1733e310180aaeb95f03b8769eb3c6da29ac070b
Eksperimental <eksperimental@autistici.org>
2022-01-11 09:37:17 -0500

    Initial commit

---
+++ lib/extended_types.ex
@@ -0,0 +1,18 @@
+defmodule ExtendedTypes do
+  @moduledoc """
+  Documentation for `ExtendedTypes`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExtendedTypes.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/extended_types_test.exs
@@ -0,0 +1,8 @@
+defmodule ExtendedTypesTest do
+  use ExUnit.Case
+  doctest ExtendedTypes
+
+  test "greets the world" do
+    assert ExtendedTypes.hello() == :world
+  end
+end
